dbe94a53af3283640d200489dd9c47ad925e2e65
Katherine Unverdi <cutterin@yahoo.com>
2016-05-18 17:20:30 -0500

    kanban_app
    Initial kanban app that has persistent data

---
+++ app/components/App.jsx
@@ -0,0 +1,48 @@
+import AltContainer from 'alt-container';
+import React from 'react';
+import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions';
+import NoteStore from '../stores/NoteStore';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <button className="add-note" onClick={this.addNote}>+</button>
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
+      </div>
+    );
+  }
+
+  deleteNote(id, e) {
+    // Avoid bubbling to edit
+    e.stopPropagation();
+
+    NoteActions.delete(id);
+  }
+  // We are using an experimental feature known as property
+  // initializer here. It allows us to bind the method `this`
+  // to point at our *App* instance.
+  //
+  // Alternatively we could `bind` at `constructor` using
+  // a line, such as this.addNote = this.addNote.bind(this);
+  addNote() {
+      NoteActions.create({task: 'New task'});
+    }
+
+  editNote(id, task) {
+    // Don't modify if trying to set an empty value
+    if(!task.trim()) {
+      return;
+    }
+    NoteActions.update({id, task});
+  }
+
+}
